f6bdaad5c51577bba74e9efffefd4c4cbbf64cda
Malin Lundin <malin@Malins-MacBook-Air.local>
2020-09-09 11:58:41 +0200

    clearstorage

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //if kollar om det finns en user eller annars inte inloggad
-if  (localStorage.getItem(username,password) == document.getElementById('username','password').value)
+let correctname = "test";
+let correctpass = "1234";
+
+if  (localStorage.user===correctname && localStorage.pass===correctpass) 
     {  
         inLog();
     } 
@@ -7,18 +10,24 @@ if  (localStorage.getItem(username,password) == document.getElementById('usernam
         href="index.html";
     }          
 
+    button.addEventListener('click', function () {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    if (username == "test" && password == "1234") {
+        localStorage.setItem("user", username);
+        localStorage.setItem("pass", password);
+        //Spara user i localStorage
+        inLog();
+    }
+    else {
+        errLog();
+    }
+});
 
 function inLog(){
     document.getElementById('header').innerHTML = ("<h1>Välkommen till din sida </h1>");
     document.getElementById('content').innerHTML = ('<p> Du är nu inloggad på din sida</p> <br> <p>Vill du logga ut </p');
     document.getElementById('but').innerHTML = ("<input type='button' value='logga ut' onClick='localStorage.clear(); outLog().innerHTML;'/>");
-    }
-
-function outLog(){
-    document.getElementById('header').innerHTML = ("<h1> Du är nu utloggad</h1>");
-    document.getElementById('content').innerHTML = ("<p>Vill du logga in igen <a href='index.html'>Logga in</a> </p>")
-    document.getElementById('but').innerHTML = ('');
-    //tömma localStorage
     }
 
 function errLog(){
@@ -27,16 +36,13 @@ function errLog(){
    document.getElementById('but').innerHTML = ('');
    }
 
-  button.addEventListener ('click', function (){
-    let username=document.getElementById('username').value
-    let password=document.getElementById('password').value
-    if(username=="test" && password=="1234"){
-    localStorage.setItem("user", username);
-    localStorage.setItem("pass", password);
-        //Spara user i localStorage
-        inLog();
+function outLog(){
+    document.getElementById('header').innerHTML = ("<h1> Du är nu utloggad</h1>");
+    document.getElementById('content').innerHTML = ("<p>Vill du logga in igen <a href='index.html'>Logga in</a> </p>")
+    document.getElementById('but').innerHTML = ('');
+   
     }
-    else {  
-        errLog();
-    }
-})
+
+
+  
+
